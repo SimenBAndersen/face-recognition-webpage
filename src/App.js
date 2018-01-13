7e5a4bb4df8afd51f979afbfd6d0ad85cc65aef3
Simen Bjerkeset Andersen <simen-bandersen@hotmail.no>
2018-01-13 21:18:34 +0100

    Added API and basic state + event handlers
    Clarifai API included to recognize faces. Still needs to add code to preview this for the user.
    
    Event handlers for input and button added. Doesn't do anything yet, but ready to be added logic.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: 'dfd1ce991fd840bf861cc32aa33d458a'
+});
 
 const particlesOptions = {
   particles: {
@@ -19,6 +24,29 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.setState= {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('Submitted');
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
+
   render() {
     return (
       <div className="App">
@@ -26,7 +54,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/*<FaceRecognition />*/}
       </div>
     );
